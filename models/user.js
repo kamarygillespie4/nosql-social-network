5197a67045de0bc1ce5eb0a1977fede15a50a47f
Kamary <kamarygillespie@Kamarys-MBP.lan>
2022-09-27 12:40:46 -0600

    created user model with todos to figure out

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+//TODO: require thought model
+
+// Schema to create Student model
+const studentSchema = new Schema({
+        username: {
+            //is a string
+            type: String,
+            //is required
+            required: true,
+            //is unique
+            unique: true,
+            //TODO: make trimmed
+            //
+        },
+        email: {
+            //is a string
+            type: String,
+            //is required
+            required: true,
+            //is unique
+            unique: true,
+            //TODO: Must match a valid email address (look into Mongoose's matching validation)
+        },
+        thoughts: {
+            //TODO: Array of _id values referencing the Thought model
+        },
+        friends: {
+            //TODO:Array of _id values referencing the User model (self-reference)
+        },
+    },
+    // {
+    //   toJSON: {
+    //     getters: true,
+    //   },
+    // }
+
+    //TODO:Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+);
+
+const Student = model('student', studentSchema);
+
+module.exports = Student;
